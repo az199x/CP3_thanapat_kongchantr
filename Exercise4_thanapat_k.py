ff3e498f0d53acf680485483acd99e86cb2fea87
az199x <126133960+az199x@users.noreply.github.com>
2024-11-30 03:36:01 +0700

    Add files via upload

---
+++ Exercise4_thanapat_k.py
@@ -0,0 +1,11 @@
+fe = float(input("Foundation English / enter you score :"))
+ge = float(input("General Business / enter you score :"))
+IC = float(input("Introduction to computer / enter you score :"))
+CP = float(input("Computer Programming / enter you score :"))
+
+print("====Your score====")
+
+print("Foundation English :",fe)
+print("General Business :",ge)
+print("Introduction to computer :",IC)
+print("Computer Programming :",CP)
