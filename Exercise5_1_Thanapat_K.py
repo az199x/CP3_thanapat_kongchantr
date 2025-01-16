ebf43c28b64f63c8e049abc9b57581864f567f8a
az199x <126133960+az199x@users.noreply.github.com>
2025-01-17 03:39:42 +0700

    Add files via upload

---
+++ Exercise5_1_Thanapat_K.py
@@ -0,0 +1,6 @@
+a = int(input('enter num :'))
+b = int(input('enter num :'))
+print(a,'+',b,'=',a+b)
+print(a,'+',b,'=',a-b)
+print(a,'+',b,'=',a*b)
+print(a,'+',b,'=',int(a/b))
---
+++ Exercise5_2_Thanapat_k.py
@@ -0,0 +1,4 @@
+s = int(input('enter speed :'))
+t = int(input('enter time :'))
+v = s/t
+print(int(v),'km/h')
